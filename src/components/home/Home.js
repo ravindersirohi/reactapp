0986a5a11e2812e244a5d5dc44ad7da2e931aa23
Ravinder Sirohi <Ravinder.Sirohi@hovergen.com>
2020-04-23 22:49:37 +0100

    Action buttons added.

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,22 +1,28 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Card, CardBody, CardTitle, CardFooter, Button } from 'reactstrap';
 import './home.scss';
 import Profile from '../profile/Profile';
 
 function Home() {
-  const submitHandler = event => {
-    console.log('Submit Handler!');
+  const profileRef = useRef();
+  const saveEvent = event => {
+    const info = profileRef.current();
+    console.log(info);
+    //Todo - API call to save form data.
+  }
+  const resetEvent = event => {
+    profileRef.current('reset');
   }
   return (
     <div className="home">
       <Card>
         <CardBody>
           <CardTitle>Profile</CardTitle>
-          <Profile />
+          <Profile ref={profileRef} />
         </CardBody>
         <CardFooter>
-          <Button>Cancel</Button>
-          <Button type="submit" onClick={submitHandler}>Save</Button>
+          <Button type="button" onClick={resetEvent}>Reset</Button>
+          <Button type="submit" onClick={saveEvent}>Save</Button>
         </CardFooter>
       </Card>
     </div>
--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,11 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './profile.scss';
 
-function Profile() {
+const Profile = React.forwardRef((props, ref) => {
     const [info, setInfo] = useState({ fullName: '', email: '', address: '' });
     const setFullName = event => setInfo({ ...info, fullName: event.target.value });
     const setEmail = event => setInfo({ ...info, email: event.target.value });
     const setAddress = event => setInfo({ ...info, address: event.target.value });
+    useEffect(() => {
+        ref.current = event => {
+            if (event === 'reset')
+                setInfo({ fullName: '', email: '', address: '' });
+            else
+                return info;
+        }
+    }, [ref, info]);
     return (
         <div className="profile">
             <div className="input-group">
@@ -20,11 +28,11 @@ function Profile() {
             </div>
             <div className="input-group">
                 <label className="input-group-text">Address</label>
-                <input type="text" value={info.address} className="form-control" placeholder="Bond street"
+                <input type="text" value={info.address} className="form-control" placeholder="7 Bond street, London"
                     onChange={setAddress} />
             </div>
         </div>
     );
-}
+})
 
 export default Profile;
