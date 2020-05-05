ca07f89612fb91fa273576a49334cef7ba15092b
Ravinder Sirohi <Ravinder.Sirohi@hovergen.com>
2020-05-05 02:02:16 +0100

    Redux Logger middleware added.

--- src/store.js
+++ src/store.js
@@ -1,8 +1,11 @@
 import { createStore, applyMiddleware } from 'redux';
-import profileReducer from './reducers/ProfileReducer';
+import { logger } from 'redux-logger'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
-const middleware = [thunk]
+import profileReducer from './reducers/ProfileReducer';
+
+const middleware = [thunk, logger]
+
 const store = createStore(
     profileReducer,
     composeWithDevTools(applyMiddleware(...middleware))
